dba63823971260cf080eaeadf498eeab17ca447f
Ernesto Ledesma <eeledesma@teco.com.ar>
2025-05-15 08:18:50 -0300

    proyecto finalizado

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,23 +1,27 @@
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
 
+const Character =  async () => {
 
-const Character = () => {
- const view = `
-    <div class="Characters-inner">
-        <article class="Character-card">
-            <img src="/" alt="Character Name">
-            <h2>Character Name</h2>
-        </article>
+    const id = getHash();
+    const character = await getData(id);
+    const view = `
+        <div class="Characters-inner">
+            <article class="Character-card">
+                <img src="${character.image}" alt="${character.name}">
+                <h2>${character.name}</h2>
+            </article>
 
-        <article class="Character-card">
-            <h3>Episodes:</h3>
-            <h3>Status:</h3>
-            <h3>Species:</h3>
-            <h3>Gender:</h3>
-            <h3>Origin:</h3>
-            <h3>Last Location:</h3>
-        </article>
-    </div>
- `
-}
-
+            <article class="Characters-card">
+                <h3>Episodes: <span>${character.episode.length}</span></h3>
+                <h3>Status:<span>${character.status}</span></ </h3>
+                <h3>Species: <span>${character.species}</span></</h3>
+                <h3>Gender: <span>${character.gender}</span></</h3>
+                <h3>Origin: <span>${character.origin.name}</span></</h3>
+                <h3>Last Location: <span>${character.location.name}</span></</h3>
+            </article>
+        </div> 
+    `
+    return view
+};
 export default Character;
